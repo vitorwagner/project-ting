d84b6abececef76a5cab97a249a870f2727d0bfc
Vitor Wagner <vitormwagner@gmail.com>
2023-06-13 13:59:31 -0300

    Feat: :sparkles: Added `txt_importer` function at `file_management.py`

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+# https://stackoverflow.com/a/5899544
+# https://www.w3schools.com/python/ref_string_splitlines.asp
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
