f3097e830c01842241e5dab706ba8738de673fb5
Vitor Wagner <vitormwagner@gmail.com>
2023-06-13 15:38:49 -0300

    Feat: :sparkles: Added `search_by_word` function in `word_search` module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,19 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = list()
+    for i in range(0, len(instance)):
+        occurences = list()
+        for line_index, line\
+                in enumerate(instance.search(i)['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                occurences.append({'linha': line_index + 1,
+                                   'conteudo': line})
+        if occurences:
+            output.append({
+                'palavra': word,
+                'arquivo': instance.search(i)['nome_do_arquivo'],
+                'ocorrencias': occurences,
+            })
+
+    return output
