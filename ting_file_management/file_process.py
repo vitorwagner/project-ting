56cace2ae8451baace104367a3d42bf57143a3c0
Vitor Wagner <vitormwagner@gmail.com>
2023-06-13 14:23:38 -0300

    Feat: :sparkles: Adds `process` function to `file_process` module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(0, len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return None
+
+    lines = txt_importer(path_file)
+    output = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines,
+    }
+
+    instance.enqueue(output)
+    print(output, file=sys.stdout)
 
 
 def remove(instance):
