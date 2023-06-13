1d5cf4c34bde58c4e087c836d2d27f8a2b02e434
Vitor Wagner <vitormwagner@gmail.com>
2023-06-13 15:37:43 -0300

    Feat: :sparkles: Added `exists_word` function in `word_search` module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+# https://realpython.com/python-enumerate/#using-pythons-enumerate
+# https://stackoverflow.com/a/319435
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = list()
+    for i in range(0, len(instance)):
+        occurences = list()
+        for line_index, line\
+                in enumerate(instance.search(i)['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                occurences.append({'linha': line_index + 1})
+        if occurences:
+            output.append({
+                'palavra': word,
+                'arquivo': instance.search(i)['nome_do_arquivo'],
+                'ocorrencias': occurences,
+            })
+
+    return output
 
 
 def search_by_word(word, instance):
