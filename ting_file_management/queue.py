b217fc2b8858360d1f4a6816bdfdba3f2dc5a163
Vitor Wagner <vitormwagner@gmail.com>
2023-06-13 13:30:38 -0300

    Feat: :sparkles: Queue class implemented at

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,22 @@
 from ting_file_management.abstract_queue import AbstractQueue
+from collections import deque
+# https://docs.python.org/3/library/collections.html#deque-objects
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui."""
+        self._queue = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index not in range(0, self.__len__()):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._queue[index]
